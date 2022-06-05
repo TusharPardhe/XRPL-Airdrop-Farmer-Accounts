9c86ff5ffbb5bde9f251c0c64889c47da9371e06
Tushar Pardhe <tushar.pardhe@airtel.com>
2022-06-05 13:24:33 +0530

    Added UI for farmer search

---
+++ index.js
@@ -0,0 +1,34 @@
+function readTextFile(file, callback) {
+    let rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    };
+    rawFile.send(null);
+}
+
+const getFileData = () => {
+    return new Promise((resolve, reject) => {
+        readTextFile("./TokenFarmers.json", function (text) {
+            var data = JSON.parse(text);
+            resolve(data);
+        });
+    });
+};
+
+(function main() {
+    getFileData().then((data) => {
+        const input = document.getElementById("search_field");
+        const btn = document.getElementById("search_btn");
+        btn.onclick = function () {
+            if (!data[input.value]) {
+                alert("Your input is not present in the list");
+            } else {
+                alert("You're a farmer!!");
+            }
+        };
+    });
+})();
